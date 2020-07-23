ab3d3e74e0a54f97ec87bb38f7b61ecb3f7f844b
tatianamara <61988015+tatianamara@users.noreply.github.com>
2020-07-23 14:17:48 -0300

    Create exercise_7.js

---
+++ exercises_module_1/exercise_7.js
@@ -0,0 +1,7 @@
+//Exercise 7
+//Convert the following code snippet using Template Literals:
+
+const user = 'Diego';
+const age = 23;
+console.log('The user ' + user + ' is ' + age + ' years old');
+console.log(`The user ${user} is ${age} years old`) //using Template Literals
